43d6a8c0b4a521bf4d15ef12fec860a6afbd9969
DavidHaighton <72260849+DavidHaighton@users.noreply.github.com>
2022-03-11 15:27:02 -0500

    added script

---
+++ wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then  #if absolute directory is given
+        wget -O /tmp/wallpaper.jpg "http://www.bing.com/$(wget -q -O- https://binged.it/2ZButYc | sed -e 's/<[^>]*>//g' | cut -d / -f2 | cut -d \& -f1)"
+        gsettings set org.gnome.desktop.background picture-uri file:////tmp/wallpaper.jpg
+else
+        dir=$1 #get absolute directory path
+        number=$(ls $dir | wc -l) # get number of files
+        index=$(($RANDOM % $number +1)) #select a wallpaper randomly
+        file=$(ls $dir  | sed -n $(echo $index)p) #get file name of index
+        path=$(echo $dir/$file) #get absolute path to picture
+        gsettings set $(echo org.gnome.desktop.background picture-uri file:////$path) #change wallpaper
+
+fi
+
